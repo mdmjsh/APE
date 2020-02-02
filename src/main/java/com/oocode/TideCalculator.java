46abd3898bd76ab7865129dc1d2ceedb8ce96122
Ivan Moore <ivan@teamoptimization.com>
2020-02-02 16:14:36 +0000

    initial

---
+++ src/main/java/com/oocode/TideCalculator.java
@@ -0,0 +1,60 @@
+package com.oocode;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import static java.lang.Integer.parseInt;
+
+public class TideCalculator {
+    public static void main(String[] args) throws Exception {
+        System.out.println(midDayTide("Folkestone", "11-01-2020"));
+    }
+
+    // Signature of this must remain unchanged because it is relied on by the rest of the system
+    public static BigDecimal midDayTide(String place, String date) throws IOException {
+        String l;
+        Request request = new Request.Builder()
+                .url(String.format("https://dry-fjord-40481.herokuapp.com/tides/%s/%s", place, date))
+                .build();
+
+        try (Response e = new OkHttpClient.Builder().build().newCall(request).execute()) {
+            try (ResponseBody x = e.body()) {
+                assert x != null;
+                l = x.string();
+//                System.out.println(l);
+            }
+        }
+
+        String[] result = l.split("\n");
+        Value first = new Value(time(result[1].split(" ")[1]), new BigDecimal(result[1].split(" ")[2]));
+        Value second = new Value(time(result[2].split(" ")[1]), new BigDecimal(result[2].split(" ")[2]));
+        Duration between = Duration.between(first.first, second.first);
+        Duration since = Duration.between(first.first, LocalTime.NOON);
+        BigDecimal betweenLevels = second.second.subtract(first.second);
+        double proportionOfWayThrough = (double) since.toMillis() / (double) between.toMillis();
+        BigDecimal sinceLevelChange = betweenLevels.multiply(new BigDecimal(proportionOfWayThrough));
+        return first.second.add(sinceLevelChange).setScale(2, RoundingMode.CEILING);
+    }
+
+    private static LocalTime time(String time) {
+        return LocalTime.of(parseInt(time.split(":")[0]), parseInt(time.split(":")[1]));
+    }
+
+    private static class Value {
+        public final LocalTime first;
+        public final BigDecimal second;
+
+        public Value(LocalTime first, BigDecimal second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+}
---
+++ src/test/java/com/oocode/SomeTest.java
@@ -0,0 +1,13 @@
+package com.oocode;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SomeTest {
+    @Test
+    public void assignment_eg1() {
+        assertThat(1, equalTo(1));
+    }
+}

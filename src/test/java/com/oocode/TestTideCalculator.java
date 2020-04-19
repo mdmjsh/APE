1ff517cfec098fff5ebbb8128a31107586df9291
jshhrrsn <joshharrison13@gmail.com>
2020-04-19 13:11:35 +0100

    Updated mockito in build.gradle following java 11 compatibility issue

--- src/test/java/com/oocode/TestTideCalculator.java
+++ src/test/java/com/oocode/TestTideCalculator.java
@@ -1,15 +1,20 @@
 package com.oocode;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 public class TestTideCalculator {
     @Test
-    public void testInterpolateTideHeight {
-        mockMakeApiCall = mock(TideCalculator.makeApiCall())
+    public void testInterpolateTideHeight() throws IOException {
+        TideCalculator tideCalculator = mock(TideCalculator.class);
+        String knownErrorData = "LW 06:00 2.55\nHW 12:11 3.39\nLW 18:22 2.85";
 
-        assertThat(, equalTo(1));
+        when(tideCalculator.getTideTimesString("Folkestone", "12-01-2020")).thenReturn(knownErrorData);
+        assertThat(tideCalculator, equalTo(3.36));
     }
 }
+
